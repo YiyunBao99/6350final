92680579d42e8c5929673184241fc7c6eb74e9c0
Yiyun Bao <115216956+YiyunBao99@users.noreply.github.com>
2023-12-09 17:56:04 -0800

    Add files via upload

---
+++ final4/final4/WeatherClass.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherClass.swift
+//  final4
+//
+//  Created by Yiyun Bao on 12/9/23.
+//
+
+import Foundation
+
+class WeatherClass {
+    var cityCode: String = ""
+    var city: String = ""
+    var temperature: Int = 0
+    var conditions: String = ""
+}
